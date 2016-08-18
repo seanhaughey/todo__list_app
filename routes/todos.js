8877d915e47a8ef4ff41a47b488c1544555f7993
Sean Haughey <sdhaughey@gmail.com>
2016-08-18 12:13:40 -0500

    add post todos route

--- routes/todos.js
+++ routes/todos.js
@@ -13,4 +13,15 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.post('/', (req, res, next) => {
+  const todo = new Todo({ task: req.body.task, completed: false });
+  todo.save(function (err) {
+    if (err){
+      res.status(500).send()
+    } else {
+      res.json(todo);
+    }
+  })
+});
+
 module.exports = router;
